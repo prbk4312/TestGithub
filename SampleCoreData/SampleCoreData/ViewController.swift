5f7dd653b83600b0eff50229ea678446a81f2b5c
PrabhuKannan <prbk4312@gmail.com>
2018-05-04 12:20:09 +0530

    test commit 4

--- SampleCoreData/SampleCoreData/ViewController.swift
+++ SampleCoreData/SampleCoreData/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // test comment inside view controller
+        // test changes done for Dev_Temp branch
     }
 
     override func didReceiveMemoryWarning() {
